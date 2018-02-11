55e3dc1c8fa1c382057f164c39f09d188ba96a75
eran132 <eran132@gmail.com>
2018-02-11 12:10:09 +0200

    Added content to default.rb

---
+++ class1/cookbooks/opsschool-logo/attributes/default.rb
@@ -0,0 +1,5 @@
+default['nginx']['port'] = 80
+default['nginx']['default_root'] = '/opt/opsschool'
+#default['nginx']['logo_url'] = 'https://static.wixstatic.com/media/ba2a09_b5ebfce1a107401c81962d1b4eb67b4a~mv2.png/v1/fill/w_214,h_108,al_c,lg_1/ba2a09_b5ebfce1a107401c81962d1b4eb67b4a~mv2.png'
+default['nginx']['user'] = 'root'
+default['nginx']['default_site_enabled']   = true
--- class1/cookbooks/opsschool-logo/metadata.rb
+++ class1/cookbooks/opsschool-logo/metadata.rb
@@ -17,3 +17,7 @@ version '0.1.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/opsschool-logo' if respond_to?(:source_url)
+
+#depends 'apt', '~> 6.1.4'
+#depends 'nginx', '~> 7.0.2'
+#depends 'seven_zip', '~> 2.0.2'
